a151358d09f8422a0a6e11cc5aad0c44fae98327
kingsksinha <84366106+kingsksinha@users.noreply.github.com>
2023-09-30 19:16:00 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -24,7 +24,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
           title: "I AM ALIVE",
           body: "GURU BOT",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp',
+          sourceUrl: 'https://chat.whatsapp.com/EKrrqxCPNN706Vjir52fKr',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
